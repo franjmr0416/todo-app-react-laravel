25090db4fccd5173016a7351ff9dd122831f65d6
Francisco <francisco@pop-os.localdomain>
2022-11-22 22:01:07 -0600

    add: todos api crud

--- api-todo/app/Http/Controllers/Api/TodoController.php
+++ api-todo/app/Http/Controllers/Api/TodoController.php
@@ -3,62 +3,47 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        $todos = Todo::all();
+        return $todos;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $todo = new Todo();
+        $todo->content = $request->content;
+        $todo->completed = $request->completed;
+        $todo->deadline = $request->deadline;
+
+        $todo->save();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $todo = Todo::find($id);
+        return $todo;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $todo = Todo::findOrFail($id);
+        $todo->content = $request->content;
+        $todo->completed = $request->completed;
+        $todo->deadline = $request->deadline;
+
+        $todo->save();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $todo = Todo::destroy($id);
+        return $todo;
     }
 }
